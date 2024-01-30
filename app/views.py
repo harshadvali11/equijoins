b5a4217fb5dfe6d0d5eff240f85ea13cc3ed03b5
jspmern <jspmern@gmail.com>
2024-01-30 18:34:13 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,5 +4,23 @@ from django.shortcuts import render
 from app.models import *
 def equijoins(request):
     EMPOBJECTS=Emp.objects.select_related('deptno').all()
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(hiredate__year=2024)
+    
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(hiredate__year=2024,sal__gt=2500)
+    
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(deptno=10)
+    
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(deptno__dname='ACCOUNTING')
+    EMPOBJECTS=Emp.objects.select_related('deptno').all()
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(deptno__dlocation='DALLAS')
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(mgr__isnull=True)
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(comm__isnull=True)
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(comm__isnull=False)
+    EMPOBJECTS=Emp.objects.select_related('deptno').all()[2:5:]
+    
     d={'EMPOBJECTS':EMPOBJECTS}
     return render(request,'equijoins.html',d)
+
+
+
+
